2ca83ead0e4ed0f8ceb6ee08d831ece38037946d
yannick Kacou <kacou@sendchap.ci>
2025-10-11 11:54:58 +0000

    fix: wip

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,47 +1,52 @@
 // import { refreshToken } from '@/modules/auth/auth-service'
-import axios, { AxiosError, HttpStatusCode, type AxiosInstance } from 'axios'
-import router from '@/router'
+import axios, { AxiosError, HttpStatusCode, type AxiosInstance } from "axios";
+import router from "@/router";
 
-import { destroySensitiveInfo, getAccessToken, getBearerToken, getDeviceId } from '@/services/auth/auth-util'
-import { env } from './env'
-import { logout, refreshToken } from '@/services/auth/auth-service'
-import { AppRoute } from '@/constants/app-route'
-import { useLoaderStore } from '@/stores/useLoaderStore'
+import {
+  destroySensitiveInfo,
+  getAccessToken,
+  getBearerToken,
+  getDeviceId,
+} from "@/services/auth/auth-util";
+import { env } from "./env";
+import { logout, refreshToken } from "@/services/auth/auth-service";
+import { AppRoute } from "@/constants/app-route";
+import { useLoaderStore } from "@/stores/useLoaderStore";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: env.VITE_API_URL,
   // withCredentials: true,
   headers: {
-    Accept: 'application/json',
+    Accept: "application/json",
     // 'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
-})
+});
 
 axiosInstance.interceptors.request.use(
   async (config) => {
-    const token = getAccessToken() // Retrieve access token
+    const token = getAccessToken(); // Retrieve access token
     if (token) {
-      config.headers['Device-Id'] = getDeviceId() // Add device ID
-      config.headers['Authorization'] = `Bearer ${token}` // Add authorization header
+      config.headers["Device-Id"] = getDeviceId(); // Add device ID
+      config.headers["Authorization"] = `Bearer ${token}`; // Add authorization header
     }
-    return config
+    return config;
   },
   (error) => {
-    return Promise.reject(error)
+    return Promise.reject(error);
   },
-)
+);
 
 // Response Interceptor
 axiosInstance.interceptors.response.use(
   (response) => {
-    return response // Return the response as-is
+    return response; // Return the response as-is
   },
   async (error) => {
-   const res: ErrorResponse = error?.response?.data
-    const originalRequestConfig = error.config
-    const { stopLoading } = useLoaderStore()
-    stopLoading()
+    const res: ErrorResponse = error?.response?.data;
+    const originalRequestConfig = error.config;
+    const { stopLoading } = useLoaderStore();
+    stopLoading();
 
     // if (
     //   error?.status === HttpStatusCode.Unauthorized ||
@@ -55,7 +60,8 @@ axiosInstance.interceptors.response.use(
 
     // Handle token expiration
     if (
-      (error?.status === HttpStatusCode.Unauthorized || error?.status === HttpStatusCode.NotAcceptable) 
+      error?.status === HttpStatusCode.Unauthorized ||
+      error?.status === HttpStatusCode.NotAcceptable
       // && !originalRequestConfig._isRetry
     ) {
       try {
@@ -63,29 +69,28 @@ axiosInstance.interceptors.response.use(
         // originalRequestConfig._isRetry = true
 
         // Refresh the token
-        const refreshResponse = await refreshToken()
+        const refreshResponse = await refreshToken();
         if (refreshResponse) {
           // Update the authorization header with the new token
-          originalRequestConfig.headers['Authorization'] = getBearerToken()
-          return axios.request(originalRequestConfig) // Retry the original request
+          originalRequestConfig.headers["Authorization"] = getBearerToken();
+          return axios.request(originalRequestConfig); // Retry the original request
         }
       } catch (refreshError) {
         // Handle refresh token failure (e.g., redirect to login)
-        console.error('Token refresh failed:', refreshError)
-        return Promise.reject(refreshError)
+        destroySensitiveInfo();
+        router.push({ name: AppRoute.LOGIN.name, replace: true });
+        return Promise.reject(refreshError);
       }
-    }
-
-    else {
+    } else {
       if (router.currentRoute.value.name !== AppRoute.LOGIN.name) {
-        destroySensitiveInfo()
-        router.push({ name: AppRoute.LOGIN.name, replace: true })
+        destroySensitiveInfo();
+        router.push({ name: AppRoute.LOGIN.name, replace: true });
       }
     }
 
     // Reject all other errors
-    return Promise.reject(error)
+    return Promise.reject(error);
   },
-)
+);
 
-export default axiosInstance
+export default axiosInstance;
