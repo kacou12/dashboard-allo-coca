2ffd996f58e7a501c692709f376f271b9173150f
Kacou Yannick <kacou@sendchap.ci>
2025-10-29 19:23:31 +0000

    feat: add paid to order status

--- src/services/allococa/orders/order-type.ts
+++ src/services/allococa/orders/order-type.ts
@@ -1,32 +1,31 @@
-import type { StatusConfigEnum } from "@/services/global.type"
+import type { StatusConfigEnum } from "@/services/global.type";
 
 export type OrderUpdatePayload = {
-  customer_name?: string
-  contact_client?: string
-  commune?: string
-  status?: string
-  order_date?: string
-}
+  customer_name?: string;
+  contact_client?: string;
+  commune?: string;
+  status?: string;
+  order_date?: string;
+};
 
 export type OrderCreatePayload = {
-  customer_name: string
-  contact_client: string
-  commune: string
-}
+  customer_name: string;
+  contact_client: string;
+  commune: string;
+};
 
 export type OrderStatusUpdatePayload = {
-  status?: string
-}
+  status?: string;
+};
 
 export enum OrderStatusEnum {
-  IN_PROGRESS = 'En cours',
-  DELIVERED = 'Livrée',
-  CANCELED = 'Annulée'
+  IN_PROGRESS = "En cours",
+  DELIVERED = "Livrée",
+  CANCELED = "Annulée",
 }
 
-
 // export const OrderStatusConfigEnum ={
-//   name:"PENDING", 
+//   name:"PENDING",
 //   data: { value: 'pending', name: 'En attente', color: '#6c757d', textColor: '#ffffff' },
 //   PROCESSING: { value: 'processing', name: 'En traitement', color: '#ffc107', textColor: '#000000' },
 //   PAID: { value: 'paid', name: 'Payée', color: '#28a745', textColor: '#ffffff' },
@@ -50,72 +49,91 @@ export type OrderFiltersRequest = {
   status?: string;
 };
 
-export const orderStatusConfigEnumData: StatusConfigEnum[] =[
-  { value: 'pending', name: 'En cours ', color: '#6c757d', textColor: '#ffffff' },
-  { value: 'delivered', name: 'Livrée', color: '#28a745', textColor: '#ffffff' },
-  { value: 'cancelled', name: 'Annulée', color: '#dc3545', textColor: '#ffffff' },
+// export const orderStatusConfigEnumData: StatusConfigEnum[] =[
+//   { value: 'pending', name: 'En cours ', color: '#6c757d', textColor: '#ffffff' },
+//   { value: 'delivered', name: 'Livrée', color: '#28a745', textColor: '#ffffff' },
+//   { value: 'cancelled', name: 'Annulée', color: '#dc3545', textColor: '#ffffff' },
+// ];
+
+export const orderStatusConfigEnumData: StatusConfigEnum[] = [
+  {
+    value: "pending",
+    name: "En cours ",
+    color: "#6c757d",
+    textColor: "#ffffff",
+  },
+  {
+    value: "delivered",
+    name: "Livrée",
+    color: "#28a745",
+    textColor: "#ffffff",
+  },
+  {
+    value: "cancelled",
+    name: "Annulée",
+    color: "#dc3545",
+    textColor: "#ffffff",
+  },
+  { value: "paid", name: "Payée", color: "#007bff", textColor: "#ffffff" }, // Nouveau type ajouté
 ];
 
-
 export type OrderResponse = {
-  id: string
-  delivery: Delivery
-  reference?: string
-  status: string
-  total_amount: number
-  is_settled: boolean,
-  number_of_casier: number,
-  items: OrderLine[]
-  user_id: string
-  user: User
-  order_date: string
-}
+  id: string;
+  delivery: Delivery;
+  reference?: string;
+  status: string;
+  total_amount: number;
+  is_settled: boolean;
+  number_of_casier: number;
+  items: OrderLine[];
+  user_id: string;
+  user: User;
+  order_date: string;
+};
 
-type Delivery ={
-  id: string
-  full_name: string
-  phone_number: string
-  municipality: string
-  address: string
-  notes: string
-  created_at: string
-  updated_at: string
-}
+type Delivery = {
+  id: string;
+  full_name: string;
+  phone_number: string;
+  municipality: string;
+  address: string;
+  notes: string;
+  created_at: string;
+  updated_at: string;
+};
 
 type OrderLine = {
-  id: string
-  type: string
-  variant: Variant
-  quantity: number
-  unit_price: number
-  line_total: number
-}
+  id: string;
+  type: string;
+  variant: Variant;
+  quantity: number;
+  unit_price: number;
+  line_total: number;
+};
 
 type Variant = {
-  id: string
-  name: string
-  description:string
-  label:string
-  product: Product
-}
+  id: string;
+  name: string;
+  description: string;
+  label: string;
+  product: Product;
+};
 
- type Product = {
-  id: string
-  name: string
-}
+type Product = {
+  id: string;
+  name: string;
+};
 
-
-
- type User = {
-  name: string
-  email: string
-  phone: string
-  address: string
-  role: string
-  id: string
-  created_at: string
-  updated_at: string
-  last_login: string
-  created_by: string
-  updated_by: string
-}
+type User = {
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  role: string;
+  id: string;
+  created_at: string;
+  updated_at: string;
+  last_login: string;
+  created_by: string;
+  updated_by: string;
+};
